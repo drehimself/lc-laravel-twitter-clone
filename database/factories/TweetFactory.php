0bd0585ca95065ffbb33c286cb4f4c36f574d875
Andre Madarang <andre_madarang@hotmail.com>
2021-09-16 18:49:21 -0400

    Create Tweet model and starting data

---
+++ database/factories/TweetFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tweet;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TweetFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Tweet::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'body' => $this->faker->sentence(20),
+            'created_at' => $this->faker->dateTimeBetween('-30 days', now()),
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tweet;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Tweet::factory(40)->create();
+    }
+}
