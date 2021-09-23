8401326561d46a649a4c60883f211757cc782883
Andre Madarang <andre_madarang@hotmail.com>
2021-09-22 21:38:18 -0400

    Add endpoint for adding a new tweet

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,14 @@ Route::get('/tweets', function () {
 Route::get('/tweets/{tweet}', function (Tweet $tweet) {
     return $tweet->load('user:id,name,username,avatar');
 });
+
+Route::post('/tweets', function (Request $request) {
+    $request->validate([
+        'body' => 'required',
+    ]);
+
+    return Tweet::create([
+        'user_id' => 1,
+        'body' => $request->body,
+    ]);
+});
